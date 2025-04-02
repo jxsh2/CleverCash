2287be47ca60c4672856e7091c5b7a4397778e6d
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2025-04-03 05:12:26 +0800

    fix url

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 
 const BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1/";
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api/";
 
 const GlobalContext = React.createContext();
 
