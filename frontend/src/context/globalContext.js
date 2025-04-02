00fd5bdb85f817eeee8f3817bef96e0d406d70d7
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2025-04-03 05:01:50 +0800

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000/api/v1/";
 
 const GlobalContext = React.createContext();
 
