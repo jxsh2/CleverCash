5ebeb2baa0b00ba9b5d2bfed2288f8bb87e8202e
jxsh2 <92442082+jxsh2@users.noreply.github.com>
2025-04-05 17:51:03 +0800

    app.js updates

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -2,7 +2,6 @@ import React, { useContext, useState } from "react";
 import axios from "axios";
 
 const BASE_URL = "https://clever-cash-backend.vercel.app/api/v1/";
-
 // "http://localhost:5000/api/v1/";
 
 const GlobalContext = React.createContext();
